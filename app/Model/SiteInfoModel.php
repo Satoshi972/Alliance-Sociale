611bc51d8e0e8b1ba3cfa0cad3b3f9b1c3c86717
Ruddy MARIE-LUCE <ruddy.marieluce@gmail.com>
2017-04-09 16:42:24 -0400

    sauvegarde du model

--- app/Model/SiteInfoModel.php
+++ app/Model/SiteInfoModel.php
@@ -1,1 +1,27 @@
-SiteInfoModel.php
+<?php 
+namespace Model;
+
+use \W\Model\Model as Model;
+
+class SiteInfo extends Model
+{
+	
+	public function siteIfo()
+	{
+		$sql = 'SELECT M.url, SI.* FROM medias as M, media_of as MO, site_info as SI WHERE SI.id = MO.id_related AND M.id = MO.id_media';
+		$sth = $this->dbh->prepare($sql);
+		$sth->execute();
+
+		return $sth->fetch();
+	}
+
+	public function UpdateInfo($data, $id)
+	{
+		$sql = 'UPDATE site_info SET ';
+		foreach($data as $key => $value){
+			$sql .= "`$key` = :$key, ";
+		}
+	}
+
+}
+?>
