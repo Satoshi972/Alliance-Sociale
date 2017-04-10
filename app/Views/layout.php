f5595520d061307538719aa17abcc347ceac43fd
Ruddy MARIE-LUCE <ruddy.marieluce@gmail.com>
2017-04-09 21:07:18 -0400

    Revert "Ajout de jquery"
    This reverts commit 83284b7d60585381828be8db78783421c131ba7d.

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -6,10 +6,6 @@
 
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style_modal.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
-	<link rel="stylesheet" href="<?= $this->assetUrl('js/jquery.modal.min.css') ?>">
-
-	<!-- Permet d'ajouté des entete dans ma vue -->
-	<?= $this->section('head') ?>
 	
 </head>
 <body>
@@ -31,18 +27,7 @@
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
   crossorigin="anonymous"></script>
   
-  <script
-	  src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"
-	  integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E="
-	  crossorigin="anonymous"></script>
-		
-
 <script src="<?= $this->assetUrl('js/script_modal.js') ?>"></script>
-<script src="<?= $this->assetUrl('js/jquery.modal.min.js') ?>"></script>
-
-<!-- Permet d'inserer des script dans ma vue -->
-<?= $this->section('script') ?>
-
 
 </body>
 </html>
