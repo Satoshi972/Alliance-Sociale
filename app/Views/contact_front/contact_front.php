c99b62bb3d84fef5af6603ae304562cbf9e967e6
JordanDestin <destin.jord@gmail.com>
2017-04-12 07:59:58 -0400

    view contact front
    Affichage des erreurs et des success

--- app/Views/contact_front/contact_front.php
+++ app/Views/contact_front/contact_front.php
@@ -5,38 +5,55 @@
 <div class="col-sm-12">
 	<h1>CONTACT</h1>
 </div>
+	<!-- Affichage success et errors -->
+	<div class="col-md-6 col-md-offset-3">
+		<?php if($success == true): // La variable $success est envoyé via le controller?>
+			<p style="color:green">Bravo, vos médias ont bien été enregistré</p>
+		<?php endif; ?>
 
-<div class="col-md-6 col-md-offset-3">
+		<?php if(!empty($errors)): // La variable $errors est envoyé via le controller?>
+			<p style="color:red"><?=implode('<br>', $errors); ?></p>
+		<?php endif; ?>
+	</div> 
 
-	<form method="post" id="contact" class="form-horizontal">
+	<!--  -->
 
-		<div class="form-group">
-			<label class="col-md-2 control-label" for="title">Titre</label>
-			<div class="col-md-10">
-				<input type="text" id="title" name="title" class="form-control">
+	
+
+
+
+	<!-- formulaire -->
+	<div class="col-md-6 col-md-offset-3">
+
+		<form method="post" id="contact" class="form-horizontal">
+
+			<div class="form-group">
+				<label class="col-md-2 control-label" for="title">Titre</label>
+				<div class="col-md-10">
+					<input type="text" id="title" name="title" class="form-control">
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group">
-			<label class="col-md-2 control-label" for="email">Email</label>
-			<div class="col-md-10">
-				<input type="email" id="email" name="email" class="form-control">
+			<div class="form-group">
+				<label class="col-md-2 control-label" for="email">Email</label>
+				<div class="col-md-10">
+					<input type="email" id="email" name="email" class="form-control">
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group">
-			<label class="col-md-2 control-label" for="content">Contenu</label>
-			<div class="col-md-10">
-				<input type="text" id="text" name="content" rows="5" class="form-control">
+			<div class="form-group">
+				<label class="col-md-2 control-label" for="content">Contenu</label>
+				<div class="col-md-10">
+					<input type="text" id="text" name="content" rows="5" class="form-control">
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group">
-			<div class="col-md-4 col-md-offset-4">
-				<button type="submit" id="submitForm" class="btn btn-primary">Ajouter des médias</button>
+			<div class="form-group">
+				<div class="col-md-4 col-md-offset-4">
+					<button type="submit" id="submitForm" class="btn btn-primary">Ajouter des médias</button>
+				</div>
 			</div>
-		</div>
-	</form>
-</div>
+		</form>
+	</div>
 
 <?php $this->stop('main_content') ?>
