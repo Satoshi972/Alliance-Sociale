4d18a5e6f566360837f8a08e7d44d042593199c7
JordanDestin <destin.jord@gmail.com>
2017-04-12 16:26:19 -0400

    partner
    modif listpartners

--- app/Views/partners/list_partners.php
+++ app/Views/partners/list_partners.php
@@ -1,33 +1,25 @@
 <?php $this->layout('layout_back', ['title' => 'Les utilisateurs']) ?>
 
   <?php $this->start('main_content') ?>
- 
-    <table>
-      <thead>
-        <tr>
 
-          <th>Id</th>
-          <th>Nom</th>
-          <th>Logo</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
+  <div class="container">
+      <div class="col-md-12">
+
+        <h1>Listes des Partenaires</h1>
+
         <?php foreach($partners as $partner): ?>
-          <tr>
-        <th><?=$partner['id'];?></th>
-           
-            <th><?=$partner['name'];?></th>
-            
-            <th> <img src="<?=$this->assetUrl($partner['url']);?>" class="img-responsive" style="max-width: 50%; height: auto;" alt="medias"></th>
-
-            <th><a href="<?= $this->url('update_partners', ['id' => $partner['id']])?>">Modifier</a> |
-            <a href="<?= $this->url('del_partners', ['id' => $partner['id']]) ?>">Supprimer</a></th>
-          </tr>
-          <?php endforeach; ?>
-
-      </tbody>
-    </table>
+          
  
+          <div class="col-xs-3">
+            <?=$partner['name'];?>
+            
+            <img src="<?=$this->assetUrl($partner['url']);?>" class="img-responsive" style="width: 70%; height: 50%;" alt="medias" frameborder="0" scrolling="no"></th>
 
+            <a href="<?= $this->url('update_partners', ['id' => $partner['id']])?>">Modifier</a> |
+            <a href="<?= $this->url('del_partners', ['id' => $partner['id']]) ?>">Supprimer</a>
+
+          </div>
+          <?php endforeach; ?>
+      </div>
+  </div>
     <?php $this->stop('main_content') ?>
