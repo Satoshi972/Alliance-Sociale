989e1c2ee8cfcac5f1c631dde1e86fb481a9e260
JordanDestin <destin.jord@gmail.com>
2017-04-13 16:38:34 -0400

    modif img

--- app/Views/aboutFront/aboutUs.php
+++ app/Views/aboutFront/aboutUs.php
@@ -46,7 +46,7 @@
 					<div class="col-md-offset-5 col-md-3">
 						<figure>
 							<h4 class="text-center">Présidente</h4>
-							<img src="<?= $this->assetUrl('teamAS/Jean Baptiste .jpg') ?>" alt="personnel" class="img-responsive">
+							<img src="<?= $this->assetUrl('teamAS/Jean Baptiste .jpg') ?>" alt="personnel" class="img-responsive img-thumbnail">
 							<figcaption class="text-center">Mme Léa JEAN-BAPTISTE ADOLPHE</figcaption>
 						</figure>
 					</div>
@@ -54,7 +54,7 @@
 					<div class="col-md-offset-5 col-md-3">
 						<figure>
 							<h4 class="text-center">Directeur</h4>
-							<img src="<?= $this->assetUrl('teamAS/Mongis.jpg') ?>" alt="personnel" class="img-responsive">
+							<img src="<?= $this->assetUrl('teamAS/Mongis.jpg') ?>" alt="personnel" class="img-responsive img-thumbnail">
 							<figcaption class="text-center">Mr Jean-Michel MONGIS</figcaption>
 						</figure>
 					</div>
@@ -62,7 +62,7 @@
 					<div class="col-md-offset-5 col-md-3">
 						<figure>
 							<h4 class="text-center">Coordinatrice</h4>
-							<img src="<?= $this->assetUrl('teamAS/Filet Joanna.jpg') ?>" alt="personnel" class="img-responsive">
+							<img src="<?= $this->assetUrl('teamAS/Filet Joanna.jpg') ?>" alt="personnel" class="img-responsive img-thumbnail">
 							<figcaption class="text-center">Mme Joanna FILET</figcaption>
 						</figure>
 					</div>
@@ -71,7 +71,7 @@
 						<div class="col-md-3">
 							<figure>
 								<h4 class="text-center">Chargé d'Acceuil</h4>
-								<img src="<?= $this->assetUrl('teamAS/Tania Gris Desormaux.jpg') ?>" alt="personnel" class="img-responsive">
+								<img src="<?= $this->assetUrl('teamAS/Tania Gris Desormaux.jpg') ?>" alt="personnel" class="img-responsive img-thumbnail">
 								<figcaption class="text-center">Mme Tania GROS-DESORMEAUX</figcaption>
 							</figure>
 						</div>
@@ -79,7 +79,7 @@
 						<div class="col-md-3">
 							<figure>
 								<h4 class="text-center">Référente Famille</h4>
-								<img src="<?= $this->assetUrl('teamAS/Sandrine Poulin.jpg') ?>" alt="personnel" class="img-responsive">
+								<img src="<?= $this->assetUrl('teamAS/Sandrine Poulin.jpg') ?>" alt="personnel" class="img-responsive img-thumbnail">
 								<figcaption class="text-center">Mme Sandrine POULIN</figcaption>
 							</figure>
 						</div>
@@ -87,7 +87,7 @@
 						<div class="col-md-3">
 							<figure>
 								<h4 class="text-center">Chargé d'Animation</h4>
-								<img src="<?= $this->assetUrl('teamAS/Pamela Cabit.jpg') ?>" alt="personnel" class="img-responsive">
+								<img src="<?= $this->assetUrl('teamAS/Pamela Cabit.jpg') ?>" alt="personnel" class="img-responsive img-thumbnail">
 								<figcaption class="text-center">Mme Pamela CABIT</figcaption>
 							</figure>
 						</div>
@@ -97,7 +97,7 @@
 						<div class="col-md-3">
 							<figure>
 								<h4 class="text-center">Acompagnement Scolarité</h4>
-								<img src="<?= $this->assetUrl('teamAS/Lydie Milton.jpg') ?>" alt="personnel" class="img-responsive">
+								<img src="<?= $this->assetUrl('teamAS/Lydie Milton.jpg') ?>" alt="personnel" class="img-responsive img-thumbnail">
 								<figcaption class="text-center">Mme Lydie MILTON</figcaption>
 							</figure>
 						</div>
@@ -107,7 +107,7 @@
 						<div class="col-md-3">
 							<figure>
 								<h4 class="text-center">Animateur de Loisirs</h4>
-								<img src="<?= $this->assetUrl('teamAS/Dominique Rosina.jpg') ?>" alt="personnel" class="img-responsive">
+								<img src="<?= $this->assetUrl('teamAS/Dominique Rosina.jpg') ?>" alt="personnel" class="img-responsive img-thumbnail">
 								<figcaption class="text-center">Mr Dominique ROSINA</figcaption>
 							</figure>
 						</div>
@@ -115,7 +115,7 @@
 						<div class="col-md-3">
 							<figure>
 								<h4 class="text-center">Référent Jeunesse</h4>
-								<img src="<?= $this->assetUrl('teamAS/Roger Gabrit.jpg') ?>" alt="personnel" class="img-responsive">
+								<img src="<?= $this->assetUrl('teamAS/Roger Gabrit.jpg') ?>" alt="personnel" class="img-responsive img-thumbnail" >
 								<figcaption class="text-center">Mr Roger GABRIT</figcaption>
 							</figure>
 						</div>
