43afed943427b5190f9e9d7a1ca6049619255d67
JordanDestin <destin.jord@gmail.com>
2017-04-13 07:49:25 -0400

    partners
    supppression ok

--- app/Views/partners/del_partners.php
+++ app/Views/partners/del_partners.php
@@ -1,12 +1,22 @@
 <?php 
 
-$this->layout('layout', ['title' => 'Partenaire supprimé']);
+$this->layout('layout_back', ['title' => 'Partenaire supprimé']);
 
 $this->start('main_content');
 
 ?>
 
-<input type="bouton" value="Liste des utilisateurs" onclick="document.location.href='/Alliance-Sociale/public/partners/list';">
+<div class ="container">
+	<div class="row">
+		<div class="col-md-12">
+
+			<div class="col-md-6 col-md-offset-3 text-center well">
+
+				<input type="bouton" value="Liste des utilisateurs" onclick="document.location.href='/Alliance-Sociale/public/partners/list';">
+
+		</div>
+	</div>
+</div>
 
 <?php  $this->stop('main_content');?>
 	
