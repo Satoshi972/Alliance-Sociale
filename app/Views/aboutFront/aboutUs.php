46f2d83f9b5d36759103c88b9fcaa0ec11767155
JordanDestin <destin.jord@gmail.com>
2017-04-14 14:43:59 -0400

    lightbox
    suppression du titre

--- app/Views/aboutFront/aboutUs.php
+++ app/Views/aboutFront/aboutUs.php
@@ -129,7 +129,7 @@
   <div class="modal-content">
     <div class="modal-header">
 		<button class="close" type="button" data-dismiss="modal">×</button>
-		<h3 class="modal-title">Heading</h3>
+
 	</div>
 	<div class="modal-body">
 		
