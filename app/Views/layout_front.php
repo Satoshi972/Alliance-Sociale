a687bd69e6bcf4eec05656e1b67cdea91f6202d8
JordanDestin <destin.jord@gmail.com>
2017-04-18 09:46:39 -0400

    slider

--- app/Views/layout_front.php
+++ app/Views/layout_front.php
@@ -253,51 +253,51 @@
 
           <div class="item active">
             <div class="col-xs-12 col-sm-6 col-md-2">
-              <a href="#"><img src="https://s12.postimg.org/655583bx9/item_1_180x200.png" class="img-responsive center-block"></a>
-              <h4 class="text-center">MAYORAL SUKNJA</h4>
-              <h5 class="text-center">4000,00 RSD</h5>
+              <a href="#"><img src="<?= $this->assetUrl('img/partners/sa.png') ?>" class="img-responsive center-block"></a>
+ <!--              <h4 class="text-center">MAYORAL SUKNJA</h4>
+             <h5 class="text-center">4000,00 RSD</h5>
+              -->            </div>
+          </div>
+
+          <div class="item">
+            <div class="col-xs-12 col-sm-6 col-md-2">
+              <a href="#"><img src="<?= $this->assetUrl('img/partners/DRJSCS.jpg') ?>" class="img-responsive center-block"></a>
+             <!--  <h4 class="text-center">MAYORAL KOŠULJA</h4>
+             <h5 class="text-center">4000,00 RSD</h5> -->
             </div>
           </div>
 
           <div class="item">
             <div class="col-xs-12 col-sm-6 col-md-2">
-              <a href="#"><img src="https://s12.postimg.org/41uq0fc4d/item_2_180x200.png" class="img-responsive center-block"></a>
-              <h4 class="text-center">MAYORAL KOŠULJA</h4>
-              <h5 class="text-center">4000,00 RSD</h5>
+              <a href="#"><img src="<?= $this->assetUrl('img/partners/Espace sud.jpg') ?>" class="img-responsive center-block"></a>
+           <!--    <span class="badge">10%</span> -->
+<!--               <h4 class="text-center">PANTALONE TERI 2</h4>
+<h5 class="text-center">4000,00 RSD</h5>
+<h6 class="text-center">5000,00 RSD</h6> -->
             </div>
           </div>
 
           <div class="item">
             <div class="col-xs-12 col-sm-6 col-md-2">
-              <a href="#"><img src="https://s12.postimg.org/dawwajl0d/item_3_180x200.png" class="img-responsive center-block"></a>
-              <span class="badge">10%</span>
-              <h4 class="text-center">PANTALONE TERI 2</h4>
-              <h5 class="text-center">4000,00 RSD</h5>
-              <h6 class="text-center">5000,00 RSD</h6>
+              <a href="#"><img src="<?= $this->assetUrl('img/partners/logo ville_du_marin.jpg') ?>" class="img-responsive center-block"></a>
+<!--               <h4 class="text-center">CVETNA HALJINA</h4>
+<h5 class="text-center">4000,00 RSD</h5> -->
             </div>
           </div>
 
           <div class="item">
             <div class="col-xs-12 col-sm-6 col-md-2">
-              <a href="#"><img src="https://s12.postimg.org/5w7ki5z4t/item_4_180x200.png" class="img-responsive center-block"></a>
-              <h4 class="text-center">CVETNA HALJINA</h4>
-              <h5 class="text-center">4000,00 RSD</h5>
+              <a href="#"><img src="<?= $this->assetUrl('img/partners/logo_caf.jpg') ?>" class="img-responsive center-block"></a>
+<!--               <h4 class="text-center">MAJICA FOTO</h4>
+<h5 class="text-center">4000,00 RSD</h5> -->
             </div>
           </div>
 
           <div class="item">
             <div class="col-xs-12 col-sm-6 col-md-2">
-              <a href="#"><img src="https://s12.postimg.org/e2zk9qp7h/item_5_180x200.png" class="img-responsive center-block"></a>
-              <h4 class="text-center">MAJICA FOTO</h4>
-              <h5 class="text-center">4000,00 RSD</h5>
-            </div>
-          </div>
-
-          <div class="item">
-            <div class="col-xs-12 col-sm-6 col-md-2">
-              <a href="#"><img src="https://s12.postimg.org/46yha3jfh/item_6_180x200.png" class="img-responsive center-block"></a>
-              <h4 class="text-center">MAJICA MAYORAL</h4>
-              <h5 class="text-center">4000,00 RSD</h5>
+              <a href="#"><img src="<?= $this->assetUrl('img/partners/logo_simar.jpg') ?>" class="img-responsive center-block"></a>
+<!--               <h4 class="text-center">MAJICA MAYORAL</h4>
+<h5 class="text-center">4000,00 RSD</h5> -->
             </div>
           </div>
 
