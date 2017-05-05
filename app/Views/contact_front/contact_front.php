37d1eeca062c43bb83920fefcb225b43091534db
Ruddy MARIE-LUCE <ruddy.marieluce@gmail.com>
2017-05-04 20:47:49 -0400

    merge du contact front

--- app/Views/contact_front/contact_front.php
+++ app/Views/contact_front/contact_front.php
@@ -119,9 +119,6 @@
                         </table>
                     </div>
 
-<<<<<<< HEAD
-				</div>
-=======
 						<div class="row">
 							<div class="col-md-6 col-md-offset-3">
 
@@ -136,10 +133,6 @@
 				                </div>
 				            </div>
 						</div>
-
->>>>>>> origin/Dev
-
-
 
 				<div class="col-md-6">
 					<div class="col-md-12 text-center">
