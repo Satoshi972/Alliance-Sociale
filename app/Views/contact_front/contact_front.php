aa0641cd7258f582c9a453f4a81f46235b77f035
Ruddy MARIE-LUCE <ruddy.marieluce@gmail.com>
2017-05-04 20:48:25 -0400

    Revert "merge du contact front"
    This reverts commit 37d1eeca062c43bb83920fefcb225b43091534db.

--- app/Views/contact_front/contact_front.php
+++ app/Views/contact_front/contact_front.php
@@ -119,6 +119,9 @@
                         </table>
                     </div>
 
+<<<<<<< HEAD
+				</div>
+=======
 						<div class="row">
 							<div class="col-md-6 col-md-offset-3">
 
@@ -133,6 +136,10 @@
 				                </div>
 				            </div>
 						</div>
+
+>>>>>>> origin/Dev
+
+
 
 				<div class="col-md-6">
 					<div class="col-md-12 text-center">
