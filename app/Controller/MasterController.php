80ee0f31e2b76699272b27c55ac3febd902dae03
Emmanuel-Laurent <e-l.robard@hotmail.fr>
2017-05-10 12:19:56 -0400

    update boite mail
    la demande de mot de passe fonctionne avec la boite gmail de emmanuel
    avec ses identifiants

--- app/Controller/MasterController.php
+++ app/Controller/MasterController.php
@@ -173,28 +173,28 @@ class MasterController extends Controller
 		//Ask for HTML-friendly debug output
 		$mail->Debugoutput = 'html';
 		//Set the hostname of the mail server
-		$mail->Host = 'smtp.mailtrap.io';
+		$mail->Host = 'smtp.gmail.com';
 		// use
 		//$mail->Host = gethostbyname('smtp.gmail.com');
 		// if your network does not support SMTP over IPv6
 		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-		$mail->Port = 2525;
+		$mail->Port = 465;
 		//Set the encryption system to use - ssl (deprecated) or tls
-		$mail->SMTPSecure = 'tls';
+		$mail->SMTPSecure = 'ssl';
 		//Whether to use SMTP authentication
 		$mail->SMTPAuth = true;
 		//Username to use for SMTP authentication - use full email address for gmail
-		$mail->Username = "d1fc96c5cb4154";
+		$mail->Username = "el.robard@gmail.com";
 		//Password to use for SMTP authentication
-		$mail->Password = "fa0e38dfe36215";
+		$mail->Password = "allinjesus77";
 		//Set who the message is to be sent from
-		$mail->setFrom('from@example.com', 'First Last');
+		$mail->setFrom('from@AllianceSocial.com', 'Alliance-Social');
 		//Set an alternative reply-to address
 		$mail->addReplyTo('replyto@example.com', 'First Last');
 		//Set who the message is to be sent to
 		$mail->addAddress($email);
 		//Set the subject line
-		$mail->Subject = 'Reset password';
+		$mail->Subject = 'Changer de mot de passe';
 		//Read an HTML message body from an external file, convert referenced images to embedded,
 		//convert HTML into a basic plain-text alternative body
 		//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
