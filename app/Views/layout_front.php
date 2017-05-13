158eab2243eeb1b68f561825e4c9990ca4b70aaa
Emmanuel-Laurent <e-l.robard@hotmail.fr>
2017-05-12 21:13:28 -0400

    up layout front
    l’espace entre le contenu et le menu est retiré

--- app/Views/layout_front.php
+++ app/Views/layout_front.php
@@ -179,9 +179,7 @@
 
     <!-- Marketing Icons Section -->
     <div class="row">
-        <div class="col-lg-12">
-        <h1 class="page-header"></h1>
-        </div>
+        
         
         <!-- main content -->
         <div class="col-md-6 col-md-push-3">
