03f27f56c8ec3ad50975b479a8afa3e9aa265a76
JordanDestin <destin.jord@gmail.com>
2017-05-13 22:04:33 -0400

    up

--- app/Views/events/viewEvent.php
+++ app/Views/events/viewEvent.php
@@ -2,67 +2,86 @@
 $this->layout('layout_back',['title' => 'Fiche de l\'event']);
 $this->start('main_content');
 ?>
-<legend class="text-center">Fiche de l'évenement</legend>
-	<div class="list-group-item text-center">
-		<h4 class="list-group-item-heading">Affiche</h4>
-		<p class="list-group-item-text"><img class="img-responsive img-rounded" src="/Alliance-Sociale/public/<?= $infos['picture'];  ?>" alt="logo"></p>
-	</div>
 
-	<div class="list-group-item text-center">
-		<h4 class="list-group-item-heading">Intitulé</h4>
-		<p class="list-group-item-text"><?=$infos['title'];?></p>
-	</div>
 
-	<div class="list-group-item text-center">
-		<h4 class="list-group-item-heading">Date<?php if($infos['end'] !== null){echo ' de début';}?></h4>
-		<p class="list-group-item-text"><?= $infos['start'];?></p>
-	</div>
+<div class ="container">
+  <div class="row">
+	  <div class="col-md-12 well">
+		  <div class="row">
+		      <div class="col-md-12 jumbotron text-center">
+		        <h2>Fiche de l'évenement</h2>
+		      </div>
+		    </div>
 
-	<?php if($infos['end'] !== null): ?>
-	<div class="list-group-item text-center">
-		<h4 class="list-group-item-heading">Date de fin</h4>
-		<p class="list-group-item-text"><?= $infos['end'];?></p>
-	</div>
-	<?php endif; ?>
+		    <div class="row">
+			    <div class="col-md-12">
+					<div class="list-group-item text-center">
+						<h4 class="list-group-item-heading" style="color: #DAA520;">Affiche</h4>
+						<p class="list-group-item-text"><img class="img-responsive" src="/Alliance-Sociale/public/<?= $infos['picture'];  ?>" alt="logo"></p>
+					</div>
 
-	<div class="list-group-item text-center">
-		<h4 class="list-group-item-heading">Description</h4>
-		<p class="list-group-item-text"><?=$infos['content'];?></p>
-	</div>
+					<div class="list-group-item text-center">
+						<h4 class="list-group-item-heading" style="color: #DAA520;">Intitulé</h4>
+						<p class="list-group-item-text" style="color: #DAA520;"><?=$infos['title'];?></p>
+					</div>
 
-	<!-- <?php 
-		foreach ($activity as $key => $value) 
-		{
-			if($value['act_id'] = $infos['id_activity']):
-	?>
-				<div class="list-group-item text-center">
-					<h4 class="list-group-item-heading">Activité de l'évènement</h4>
-					<p class="list-group-item-text"><?=$value['name'];?></p>
+					<div class="list-group-item text-center">
+						<h4 class="list-group-item-heading" style="color: #DAA520;">Date<?php if($infos['end'] !== null){echo ' de début';}?></h4>
+						<p class="list-group-item-text" style="color: #DAA520;"><?= $infos['start'];?></p>
+					</div>
+
+					<?php if($infos['end'] !== null): ?>
+					<div class="list-group-item text-center">
+						<h4 class="list-group-item-heading" style="color: #DAA520;">Date de fin</h4>
+						<p class="list-group-item-text" style="color: #DAA520;"><?= $infos['end'];?></p>
+					</div>
+					<?php endif; ?>
+
+					<div class="list-group-item text-center">
+						<h4 class="list-group-item-heading" style="color: #DAA520;">Description</h4>
+						<p class="list-group-item-text" style="color: #DAA520;"><?=$infos['content'];?></p>
+					</div>
+
+					<!-- <?php 
+						foreach ($activity as $key => $value) 
+						{
+							if($value['act_id'] = $infos['id_activity']):
+					?>
+								<div class="list-group-item text-center">
+									<h4 class="list-group-item-heading">Activité de l'évènement</h4>
+									<p class="list-group-item-text"><?=$value['name'];?></p>
+								</div>
+					<?php 
+							endif;
+						} 
+					?> -->
+					<?php if(!empty($infos['quota'])): ?>
+					<div class="list-group-item text-center">
+						<h4 class="list-group-item-heading" style="color: #DAA520;">Quota</h4>
+						<p class="list-group-item-text" style="color: #DAA520;"><?=$infos['quota'];?></p>
+					</div>
+					<?php endif; ?>
+					<br>
+
+					<div class="text-center">
+						<a href="<?=$this->url('listEvent') ?>">
+							<button class="btn btn-default">Retour a la liste</button>
+						</a>
+
+						<a href="<?=$this->url('updateEvent',['id'=>$infos['id']]) ?>">
+							<button class="btn btn-info">Modification</button>
+						</a>
+
+						<a href="<?=$this->url('deleteEvent',['id'=>$infos['id']]) ?>">
+							<button class="btn btn-danger">Suppression</button>
+						</a>
+					</div>
 				</div>
-	<?php 
-			endif;
-		} 
-	?> -->
-	<?php if(!empty($infos['quota'])): ?>
-	<div class="list-group-item text-center">
-		<h4 class="list-group-item-heading">Quota</h4>
-		<p class="list-group-item-text"><?=$infos['quota'];?></p>
-	</div>
-	<?php endif; ?>
+			</div>
+		</div>
+ 	</div>
+</div>
 
-	<div class="text-center">
-		<a href="<?=$this->url('listEvent') ?>">
-			<button class="btn btn-default">Retour a la liste</button>
-		</a>
-
-		<a href="<?=$this->url('updateEvent',['id'=>$infos['id']]) ?>">
-			<button class="btn btn-info">Modification</button>
-		</a>
-
-		<a href="<?=$this->url('deleteEvent',['id'=>$infos['id']]) ?>">
-			<button class="btn btn-danger">Suppression</button>
-		</a>
-	</div>
 
 <?php
 $this->stop('main_content');
