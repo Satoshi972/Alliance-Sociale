4b6c65005125d1b4d09b96d0f3e853d4536f334f
Emmanuel-Laurent <e-l.robard@hotmail.fr>
2017-05-14 18:43:35 -0400

    up responsive Facebook

--- app/Views/layout_front.php
+++ app/Views/layout_front.php
@@ -178,8 +178,6 @@
                 <div class="panel-body fb-place">
                   <div class="fb-page little-fb" data-href="https://www.facebook.com/AllianceSocialeduMarin/" data-width="250" data-hide-cover="false" data-show-facepile="false" data-show-posts="false">
                   </div>
-                    
-                  <div class="fb-page middle-fb" data-href="https://www.facebook.com/AllianceSocialeduMarin/" data-tabs="timeline" data-width="210" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AllianceSocialeduMarin/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AllianceSocialeduMarin/">Alliance Sociale</a></blockquote></div>  
                
                   <div class="fb-page big-fb" data-href="https://www.facebook.com/AllianceSocialeduMarin/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AllianceSocialeduMarin/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AllianceSocialeduMarin/">Alliance Sociale</a></blockquote></div>
                 </div>
