78a1788f5adccd1a29eadd1327ecabb4f7efc81a
Ruddy MARIE-LUCE <ruddy.marieluce@gmail.com>
2017-05-19 22:07:25 -0400

    Correction about front
    boucle inutile, appel de la muvaise méthode

--- app/Views/aboutFront/aboutUs.php
+++ app/Views/aboutFront/aboutUs.php
@@ -17,8 +17,7 @@
 						<div class="col-md-12 text-center">
 							<h2 style="font-family: 'Lobster';">Que sommes nous ?</h2>
 						</div>
-					<?php foreach($views as $view): ?>
-						<p style="text-align:justify; font-family: 'Merienda';"><?=$view['description'];?></p>
+						<p style="text-align:justify; font-family: 'Merienda';"><?=$views['description'];?></p>
 						<p> <a href="<?= $this->assetUrl('files/projet social 2017-2020.pdf') ?>">Retrouvez plus en détails nos projets sociaux pour 2017-2020</a></p>
 					</div>
 				</div>
@@ -43,130 +42,26 @@
 						
 
 					<div class="col-md-7">					
-						<p style="text-align:justify; font-family: 'Merienda';"><?=$view['word'];?></p>
+						<p style="text-align:justify; font-family: 'Merienda';"><?=$views['word'];?></p>
 					</div>
 				</div>
 			</section>
 
-				<?php endforeach; ?>
 			<section class="histoire">
 				<div class="row">
 					<div class="col-md-12">
 						<div class="col-md-12 text-center">
 							<h2 style="font-family: 'Lobster';">Histoire de l'association</h2>
 						</div>
-						<p style="text-align:justify; font-family: 'Merienda';"><?=$view['history'];?></p>
+						<p style="text-align:justify; font-family: 'Merienda';"><?=$views['history'];?></p>
 					</div>
 				</div>
 			</section>
             <?php } ?>
 
-            			<div class="col-md-12 text-center">
+            <div class="col-md-12 text-center">
 				<a href="<?php echo $this->url('team') ?>" style="color: white;" class="btn btn-info">Découvrez notre équipe</a>
 			</div>
-<!-- 			<section class="organigramme">
-	<div class="row">
-		<div class="col-md-12 text-center">
-			<h2>Notre équipe</h2>
-		</div>
-		
-		<div class="row">
-			<div class="col-md-12">
-
-				<div class="col-md-4">							
-					<figure>
-						<h4 class="text-center">Coordinatrice</h4>
-						<img src="<?= $this->assetUrl('teamAS/Filet Joanna.jpg') ?>" alt="personnel" class="thumbnail img-responsive">
-						<figcaption class="text-center">Mme Joanna FILET</figcaption>
-					</figure>
-				</div>
-				<div class="col-md-4">
-					<figure>
-						<h4 class="text-center">Présidente</h4>
-						<img src="<?= $this->assetUrl('teamAS/Jean Baptiste .jpg') ?>" alt="personnel" class="thumbnail img-responsive">
-						<figcaption class="text-center">Mme Léa JEAN-BAPTISTE ADOLPHE</figcaption>
-					</figure>
-				</div>
-				<div class="col-md-4">
-					<figure>
-						<h4 class="text-center">Directeur</h4>
-						<img src="<?= $this->assetUrl('teamAS/Mongis.jpg') ?>" alt="personnel" class="thumbnail img-responsive">
-						<figcaption class="text-center">Mr Jean-Michel MONGIS</figcaption>
-					</figure>								
-				</div>
-			</div>
-		</div>
-
-
-		<div class="row">
-			<div class="col-md-12">
-
-				<div class="col-md-4">							
-					<figure>
-						<h5 class="text-center">Chargé d'Acceuil</h5>
-						<img src="<?= $this->assetUrl('teamAS/Tania Gris Desormaux.jpg') ?>" alt="personnel" class="thumbnail img-responsive">
-						<figcaption class="text-center">Mme Tania GROS-DESORMEAUX</figcaption>
-					</figure>
-				</div>
-				<div class="col-md-4">
-					<figure>
-					<h5 class="text-center">Référente Famille</h5>
-					<img src="<?= $this->assetUrl('teamAS/Sandrine Poulin.jpg') ?>" alt="personnel" class="thumbnail img-responsive">
-					<figcaption class="text-center">Mme Sandrine POULIN</figcaption>
-				</figure>
-				</div>
-				<div class="col-md-4">
-					<figure>
-					<h5 class="text-center">Chargé d'Animation</h5>
-					<img src="<?= $this->assetUrl('teamAS/Pamela Cabit.jpg') ?>" alt="personnel" class="thumbnail img-responsive">
-					<figcaption class="text-center">Mme Pamela CABIT</figcaption>
-				</figure>								
-				</div>
-			</div>
-		</div>
-
-
-
-		<div class="row">
-			<div class=" col-md-12">
-
-				<div class="col-md-4">							
-
-				<figure>
-					<h5 class="text-center">Référent Jeunesse</h5>
-					<img src="<?= $this->assetUrl('teamAS/Roger Gabrit.jpg') ?>" alt="personnel" class="thumbnail img-responsive" >
-					<figcaption class="text-center">Mr Roger GABRIT</figcaption>
-				</figure>
-				</div>
-				<div class="col-md-4">
-				<figure>
-					<h5 class="text-center">Animateur de Loisirs</h5>
-					<img src="<?= $this->assetUrl('teamAS/Dominique Rosina.jpg') ?>" alt="personnel" class="thumbnail img-responsive">
-					<figcaption class="text-center">Mr Dominique ROSINA</figcaption>
-				</figure>
-				</div>
-				<div class="col-md-4">
-				<figure>
-					<h5 class="text-center">Scolarité</h5>
-					<img src="<?= $this->assetUrl('teamAS/Lydie Milton.jpg') ?>" alt="personnel" class="thumbnail img-responsive">
-					<figcaption class="text-center">Mme Lydie MILTON</figcaption>
-				</figure>							
-				</div>
-			</div>
-		</div>
-
-
-
-
-
-
-
-		
-			
-
-
-	      </div>
-         </section> -->
 	    </div>
        </div>
 
